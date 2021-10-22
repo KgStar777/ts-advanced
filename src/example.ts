bad6481e0e1865a5592fe0734f845b845a02c1af
Daniil Sychev <sych94@yandex.ru>
2021-10-22 16:22:42 +0300

    stats

---
+++ src/example.ts
@@ -0,0 +1,29 @@
+// const add = (a: number, b: number): number => a + b
+
+// add(10, 1)
+// add(10, 2)
+// add(10, 3)
+
+// class HoldNumber {
+//     data: number = 0
+// }
+
+// class HoldString {
+//     data: string = '0'
+// }
+
+// const holdNumber = new HoldNumber();
+// const holdString = new HoldString();
+
+// holdNumber.data = 333
+// holdString.data = 'aaa'
+
+// class HoldAnything<TypeOfData> {
+//     data: TypeOfData;
+// }
+
+// const holdNumber = new HoldAnything<number>()
+// holdNumber.data = 111;
+
+// const holdString = new HoldAnything<string>()
+// holdString.data = 'asd'
